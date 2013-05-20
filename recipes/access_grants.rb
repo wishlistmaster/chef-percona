f8171c084a235c6f02b8cb1c677be71e0235849f
Mike Smullin <mike@smullindesign.com>
2013-05-19 21:00:25 -0600

    allow users to optionally define node attributes which create users, databases, and grants. setting passwords inline or via encrypted data bags.

--- recipes/access_grants.rb
+++ recipes/access_grants.rb
@@ -11,7 +11,6 @@ mysql = %Q(mysql -p"#{node["percona"]["server"]["root_password"]}" -e)
 execute %Q(#{mysql} "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');")
 
 # execute grants
-
 # debian-sys-maint user for administration
 [['SELECT', 'mysql.user'], ['SHUTDOWN', '*.*']].each do |priv, loc|
   execute %Q(#{mysql} "GRANT #{priv} ON #{loc} TO '#{node["percona"]["server"]["debian_username"]}'@localhost IDENTIFIED BY '#{node["percona"]["server"]["debian_password"]}';") do
@@ -26,6 +25,36 @@ if node["percona"]["backup"]["configure"]
   execute %Q(#{mysql} "REVOKE ALL PRIVILEGES, GRANT OPTION FROM '#{node["percona"]["backup"]["username"]}'@'localhost';")
   execute %Q(#{mysql} "GRANT RELOAD, LOCK TABLES, REPLICATION CLIENT ON *.* TO '#{node["percona"]["backup"]["username"]}'@'localhost' IDENTIFIED BY '#{node["percona"]["backup"]["password"]}';")
 end
+
+# create empty databases in preparation for data import and user grants
+if node['percona']['server'].has_key? 'databases'
+  node['percona']['server']['databases'].each do |database, h|
+    sql =  "CREATE DATABASE IF NOT EXISTS \\\`#{database}\\\`"
+    sql += "  DEFAULT CHARACTER SET = #{h['charset']}" if h['charset']
+    sql += "  DEFAULT COLLATE = #{h['collate']};" if h['collate']
+    execute %Q(#{mysql} "#{sql}")
+  end
+end
+
+# create users and grant privileges
+if node['percona']['server'].has_key? 'users'
+  # fetch user passwords out of encrypted data bag
+  passwords = Chef::EncryptedDataBagItem.load('passwords', 'mysql')
+
+  node['percona']['server']['users'].each do |user, h|
+    user, host = user.split('@').map{|s| s.gsub("'",'')} if user =~ /@/
+    host ||= h['host'] || '%'
+    password = h['password'] || passwords[user]
+    if password
+      # mysql GRANT will create the user if it doesn't exist
+      # usage is actually a no-op
+      execute %Q(#{mysql} "GRANT USAGE ON *.* TO '#{user}'@'#{host}';")
+      execute %Q(#{mysql} "SET PASSWORD FOR '#{user}'@'#{host}' = PASSWORD('#{password}');")
+      h['grants'].each do |grant|
+        execute %Q(#{mysql} "GRANT #{grant} TO '#{user}'@'#{host}';")
+      end
+    end
+  end
 
 # flush privileges
 execute %Q(#{mysql} "FLUSH PRIVILEGES;")
